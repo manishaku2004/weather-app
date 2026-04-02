4f6aae4a23ca356284c70ba127f0d6853564bf75
manishaku2004 <7372manisha@gmail.com>
2026-04-02 18:57:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// alert('ok')
+const button =document.getElementById("sbtn");
+const input=document.getElementById("city_input");
+const cityName=document.getElementById("cityName");
+const cityTime=document.getElementById("cityTime");
+const cityTemp=document.getElementById("cityTemp");
+ //async =used asincronus js
+async function getData (CityName){
+
+    //call api ( fetch is return promise)
+   
+   const promise = await fetch(`http://api.weatherapi.com/v1/current.json?key=752fec28fc6746d6834180243260104&q=${CityName}&aqi=yes
+`);
+return await promise.json()
+}
+ button.addEventListener("click", async () => {
+    // console.log(input.value);
+    const value = input.value;
+    const result= await getData(value);
+    // console.log(result);
+    cityName.innerText = `${result.location.name}, ${result.location.region}- ${result.location.country}`;
+    cityTemp.innerText = `${result.current.temp_c}`;
+    cityTime.innerText= result.location.localtime;
+    
+});
+// http://api.weatherapi.com/v1/current.json?key=752fec28fc6746d6834180243260104&q=London&aqi=yes
